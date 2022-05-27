e9f99d50a80ad853043ee5c34ede34cc3cc19d5f
Bao Tran <tranbaothe@gmail.com>
2022-05-27 13:58:20 +0700

    Init app

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+const favoriteFruits: string[] = ["apple", "strawberry", "orange"];
+
+function addFruit(fruit: string) {
+  favoriteFruits.push(fruit);
+}
+
+addFruit("banana");
+console.log(`GIA TRI: ${favoriteFruits}`);
